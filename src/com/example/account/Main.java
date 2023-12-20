4af2dd664472fb149847600ec9b2d53a3a7ad550
1Max58 <>
2023-12-20 12:47:58 +0200

    The "finally" clause that return the current account ballance added after the "catch" block

--- src/com/example/account/Main.java
+++ src/com/example/account/Main.java
@@ -7,12 +7,10 @@ public class Main {
         Account account2 = new Account("Will", 200, "67890");
         System.out.println(account2.getOwner() + " has the account #" + account2.getAccountNumber() + " with balance " + account2.getBalance());
         try {
-            try {
-                System.out.println("Try to transfer sum 150 from the first account to the second one...");
-                account1.transfer(150, account2);
-            } catch (Exception e) {
-                System.out.println(e);
-            }
+            System.out.println("Try to transfer sum 150 from the first account to the second one...");
+            account1.transfer(150, account2);
+        } catch (Exception e) {
+            System.out.println(e);
         } finally {
             System.out.println("Finally balance of the first account is " + account1.getBalance());
         }
